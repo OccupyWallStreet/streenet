b52ca396e6d3279a69426d8ce870910149de9061
Thiago de Mello Bueno <thiago@madeofpeople.org>
2014-02-13 15:54:36 -0500

    removed js for past campaigns, added share link count code

--- sites/all/themes/occupy/templates/html.tpl.php
+++ sites/all/themes/occupy/templates/html.tpl.php
@@ -8,18 +8,7 @@
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body<?php print $attributes;?>>
-<script type="text/javascript">
-    window._idl = {};
-    _idl.variant = "modal";
-    _idl.campaign = "nsa";
-    (function() {
-        var idl = document.createElement('script');
-        idl.type = 'text/javascript';
-        idl.async = true;
-        idl.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'banner');
-        document.getElementsByTagName('body')[0].appendChild(idl);
-    })();
-</script>
+
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
@@ -27,10 +16,58 @@
   <?php print $page; ?>
   <?php print $page_bottom; ?>
 
-  <!--[if !(lt IE 8)]><!-->
-   <script type="text/javascript">
-     (function(){var e=document.createElement("script");e.type="text/javascript";e.async=true;e.src=document.location.protocol+"//d1agz031tafz8n.cloudfront.net/thedaywefightback.js/widget.min.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})()
-   </script>
-<!--<![endif]-->
+<!-- Piwik -->
+<!-- On Privacy and Surveillance
+We only track internal pageviews and shares
+Our share module does no tracking on its own, 
+It only connects to the service it corresponds to when you click a share link
+The only link tracking we do via js, is logging which sharelinks you click
+Our analytics are hosted by occupy.net, and we dont sell your data to marketers
+or track your behavior.
+ -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://analytics.occupy.net/";
+    _paq.push(["setTrackerUrl", u+"piwik.php"]);
+    _paq.push(["setSiteId", "21"]);
+    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+
+    var page_title = (document).attr( title );
+    console.log("page_title": page_title );
+    if( $( "#sidebars .block-owstech-share" ).length ) {
+      $( "#sidebars .block-owstech-share .icon.facebook" ).on( 'click', function(e) {
+        _paq.push(['trackPageView', page_title  + "shared on facebook via sidebar"]);
+      });
+      $( "#sidebars .block-owstech-share .icon.twitter" ).on( 'click', function(e) {
+        _paq.push(['trackPageView', page_title  + "shared on twitter via sidebar"]);
+      });
+      $( "#sidebars .block-owstech-share .icon.google-plus" ).on( 'click', function(e) {
+        _paq.push(['trackPageView', page_title  + "shared on google-plus via sidebar"]);
+      });
+      $( "#sidebars .block-owstech-share .icon.reddit" ).on( 'click', function(e) {
+        _paq.push(['trackPageView', page_title  + "shared on reddit via sidebar"]);
+      });
+    }
+
+    if( $( ".region-content .block-owstech-share" ).length ) {
+      $( ".region-content .block-owstech-share .icon.facebook" ).on( 'click', function(e) {
+        _paq.push(['trackPageView', page_title + "shared on facebook via sidebar"]);
+      });
+      $( ".region-content .block-owstech-share .icon.twitter" ).on( 'click', function(e) {
+        _paq.push(['trackPageView', page_title + "shared on twitter via sidebar"]);
+      });
+      $( ".region-content .block-owstech-share .icon.google-plus" ).on( 'click', function(e) {
+        _paq.push(['trackPageView', page_title + "shared on google-plus via sidebar"]);
+      });
+      $( ".region-content .block-owstech-share .icon.reddit" ).on( 'click', function(e) {
+        _paq.push(['trackPageView', page_title + "shared on reddit via sidebar"]);
+      });
+    }
+  })();
+</script>
 </body>
 </html>
