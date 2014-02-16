d5134cb243b72a7118e5d89a60a3a8a7ec6f42cf
Thiago de Mello Bueno <thiago@madeofpeople.org>
2014-02-15 20:07:23 -0500

    added space to page tracking code

--- sites/all/themes/occupy/js/streetnet.js
+++ sites/all/themes/occupy/js/streetnet.js
@@ -26,33 +26,33 @@ var streetnet = {};
         _paq.push(['trackGoal', 3]);
       });
       sidebar_share_widget.find(".icon.twitter" ).on( 'click', function(e) {
-        _paq.push(['trackPageView', page_title  + "shared on twitter via sidebar"]);
+        _paq.push(['trackPageView', page_title  + " shared on twitter via sidebar"]);
         _paq.push(['trackGoal', 2]);
       });
       sidebar_share_widget.find( ".icon.google-plus" ).on( 'click', function(e) {
-        _paq.push(['trackPageView', page_title  + "shared on google-plus via sidebar"]);
+        _paq.push(['trackPageView', page_title  + " shared on google-plus via sidebar"]);
         _paq.push(['trackGoal', 4]);
       });
       sidebar_share_widget.find( ".icon.reddit" ).on( 'click', function(e) {
-        _paq.push(['trackPageView', page_title  + "shared on reddit via sidebar"]);
+        _paq.push(['trackPageView', page_title  + " shared on reddit via sidebar"]);
         _paq.push(['trackGoal', 1]);
       });
     }
     if( article_footer_share_widget.length ) {
       article_footer_share_widget.find( ".icon.facebook" ).on( 'click', function(e) {
-        _paq.push(['trackPageView', page_title + "shared on facebook via article footer"]);
+        _paq.push(['trackPageView', page_title + " shared on facebook via article footer"]);
         _paq.push(['trackGoal', 7]);
       });
       article_footer_share_widget.find( ".icon.twitter" ).on( 'click', function(e) {
-        _paq.push(['trackPageView', page_title + "shared on twitter via article footer "]);
+        _paq.push(['trackPageView', page_title + " shared on twitter via article footer "]);
         _paq.push(['trackGoal', 6]);
       });
       article_footer_share_widget.find( ".icon.google-plus" ).on( 'click', function(e) {
-        _paq.push(['trackPageView', page_title + "shared on google-plus via article footer"]);
+        _paq.push(['trackPageView', page_title + " shared on google-plus via article footer"]);
         _paq.push(['trackGoal', 8]);
       });
       article_footer_share_widget.find( ".icon.reddit" ).on( 'click', function(e) {
-        _paq.push(['trackPageView', page_title + "shared on reddit via article footer"]);
+        _paq.push(['trackPageView', page_title + " shared on reddit via article footer"]);
         _paq.push(['trackGoal', 5]);
       });
     }
