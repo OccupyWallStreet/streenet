2dfc5034ef9aef5fa30432c2d24a61bdeebfc247
Thiago de Mello Bueno <thiago@madeofpeople.org>
2014-02-16 23:36:28 -0500

    changed up strings to identify share counts

--- sites/all/themes/occupy/js/streetnet.js
+++ sites/all/themes/occupy/js/streetnet.js
@@ -22,37 +22,37 @@ var streetnet = {};
     article_footer_share_widget = $( "article.node .owstech-share" );
     if( sidebar_share_widget.length ) {
       sidebar_share_widget.find(".icon.facebook" ).on( 'click', function(e) {
-        _paq.push(['trackPageView', page_title  + "shared on facebook via sidebar"]);
+        _paq.push(['trackPageView', "sidebar fb share: " + page_title ]);
         _paq.push(['trackGoal', 3]);
       });
       sidebar_share_widget.find(".icon.twitter" ).on( 'click', function(e) {
-        _paq.push(['trackPageView', page_title  + " shared on twitter via sidebar"]);
+        _paq.push(['trackPageView', "sidebar twitter share: " + page_title ]);
         _paq.push(['trackGoal', 2]);
       });
       sidebar_share_widget.find( ".icon.google-plus" ).on( 'click', function(e) {
-        _paq.push(['trackPageView', page_title  + " shared on google-plus via sidebar"]);
+        _paq.push(['trackPageView', "sidebar g+ share: " + page_title ]);
         _paq.push(['trackGoal', 4]);
       });
       sidebar_share_widget.find( ".icon.reddit" ).on( 'click', function(e) {
-        _paq.push(['trackPageView', page_title  + " shared on reddit via sidebar"]);
+        _paq.push(['trackPageView', "sidebar reddit share: " + page_title ]);
         _paq.push(['trackGoal', 1]);
       });
     }
     if( article_footer_share_widget.length ) {
       article_footer_share_widget.find( ".icon.facebook" ).on( 'click', function(e) {
-        _paq.push(['trackPageView', page_title + " shared on facebook via article footer"]);
+        _paq.push(['trackPageView', "articlefooter fb share" + page_title ]);
         _paq.push(['trackGoal', 7]);
       });
       article_footer_share_widget.find( ".icon.twitter" ).on( 'click', function(e) {
-        _paq.push(['trackPageView', page_title + " shared on twitter via article footer "]);
+        _paq.push(['trackPageView', "articlefooter twitter share" + page_title ]);
         _paq.push(['trackGoal', 6]);
       });
       article_footer_share_widget.find( ".icon.google-plus" ).on( 'click', function(e) {
-        _paq.push(['trackPageView', page_title + " shared on google-plus via article footer"]);
+        _paq.push(['trackPageView', "articlefooter g+ share" + page_title ]);
         _paq.push(['trackGoal', 8]);
       });
       article_footer_share_widget.find( ".icon.reddit" ).on( 'click', function(e) {
-        _paq.push(['trackPageView', page_title + " shared on reddit via article footer"]);
+        _paq.push(['trackPageView', "articlefooter reddit share" + page_title ]);
         _paq.push(['trackGoal', 5]);
       });
     }
