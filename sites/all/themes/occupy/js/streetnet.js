f8eecfecd56e0b5213d945f15e5b4202edf67d6c
Thiago de Mello Bueno <thiago@madeofpeople.org>
2014-02-19 21:35:33 -0500

    story filters as blocks
    put exposed occupy_views filters into a block, rerolled views feature and made requisite css and js changes
    reintroduced story delta to story context

--- sites/all/themes/occupy/js/streetnet.js
+++ sites/all/themes/occupy/js/streetnet.js
@@ -1,3 +1,6 @@
+// Avoid 'console' errors in browsers that lack a console
+(function() { var method; var noop = function noop() {}; var methods = [ 'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn' ]; var length = methods.length; var console = (window.console = window.console || {}); while (length--) { method = methods[length]; if (!console[method]) { console[method] = noop; } } }());
+
 var streetnet = {};
 
 (function ($) {
@@ -6,7 +9,7 @@ var streetnet = {};
     attach: function( context, settings ){
       streetnet.log_shares();
       streetnet.init_form_placeholders();
-      if( $(".view-ows-stories .view-filters").length ) {
+      if( $(".block-views-exp-ows-stories-stories-page").length ) {
         streetnet.enhance_story_filters();
       }
     }
